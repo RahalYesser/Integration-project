c729ad379ba5f3dfc6655d24ea1f9142299fa04f
Mohamed Lahbib <89697853+Med-Lahbib@users.noreply.github.com>
2023-01-09 01:00:50 +0100

    Delete Flutter directory

--- Flutter/lib/core/strings/messages.dart
+++
@@ -1,4 +0,0 @@
-const ADD_SUCCESS_MESSAGE = "Image ajouté avec succes";
-const DELETE_SUCCESS_MESSAGE = "Image supprimé avec succes";
-const UPDATE_SUCCESS_MESSAGE = "Image mis à jour avec succes";
-
